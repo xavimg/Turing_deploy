c7fc99979f9802d32a1a1eb477f270d51c6738e3
xavimg <55852958+xavimg@users.noreply.github.com>
2021-10-24 01:13:57 +0200

    Register User System
    Register System for users. They can create an account and directly will be fetch to MySql.

---
+++ sistema_registro_juego/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './.env'})
+
+// const controllerPages = require('./controllers/pages_controllers')
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public  ');
+app.use(express.static(publicDirectory));
+
+// Parse URL-encodes bodies ( html forms , make sure grab data from any form)
+app.use(express.urlencoded({ extended: false }))
+// Parse JSON bodies ( sent by API clients)
+app.use(express.json());
+
+console.log(__dirname);
+// Template
+app.set('view engine', 'hbs');
+
+db.connect( (err) => {
+    if (err) {
+        console.log("IMPOSIBLE", err);
+    } else {
+        console.log('MYSQL Connected...')
+    }
+
+})
+
+// Define routes
+app.use('/', require('./routes/pages_routes'));
+app.use('/auth', require('./routes/auth'));
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000")
+})
