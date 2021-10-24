ce22ae7e0021beb6186dfa867a36b8830f7e8ab7
xavimg <55852958+xavimg@users.noreply.github.com>
2021-10-24 02:02:35 +0200

    commit from command line + env.TXT

---
+++ sistema_registro_juego/controllers/auth.js
@@ -0,0 +1,50 @@
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const db = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE
+});
+exports.register = (req, res) => {
+    console.log(req.body);
+    const { name, email, password, passwordConfirmation } = req.body;
+
+    // With ? we evade SQLinjection
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, result) => {
+
+        if(error) {
+            console.log(error);
+        }
+        
+        if (result.length > 0) {
+            return res.render('register', {
+                message: 'That email is already in use'
+            });
+        } /*else if (password !== passwordConfirmation) {
+            return res.render('register', {
+                message: 'Password do not match'
+            });
+        }*/
+        
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+
+    db.query('INSERT INTO users SET ?', {userName: name, email: email, password: hashedPassword}, (error, result) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(result);
+            return res.render('register', {
+                message: 'User registered'
+            });
+        }
+
+    })
+
+    });
+
+}
