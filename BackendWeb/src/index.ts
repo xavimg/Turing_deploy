41f86a0f4a876e096256c0bbb331568aee5d2184
xavimg <55852958+xavimg@users.noreply.github.com>
2021-12-21 18:29:11 +0100

    websocket working

--- BackendWeb/src/index.ts
+++ BackendWeb/src/index.ts
@@ -19,26 +19,44 @@ app.use(userRoutes);
 
 app.listen(3000);
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3001);
+// server.listen(1234);
 
-const ws = new WebSocket.Server({server});
+// const ws = new WebSocket.Server({});
 
-ws.on('connection', (ws: WebSocket) => {
+// ws.on('connection', (ws: WebSocket) => {
 
-    console.log("new user");
+//     console.log("new user");
 
-    ws.on('message', (msg: WebSocket.Data) => {
+//     ws.on('message', (msg: WebSocket.Data) => {
 
-        if (msg.toString() === 'ok') {
-            console.log("pass");
-        } else {
-            console.log("error");
-        }
 
-    });
+//         if (msg.toString() === 'ok') {
+//             console.log("pass");
+//         } else {
+//             console.log("error");
+//         }
 
-    ws.send('connected');
-});
+//     });
 
+//     ws.send('connected')
+// });
+
+const connection = new WebSocket("ws://192.168.233.195:1234");
+
+
+
+connection.onopen = () => {
+    
+    console.log("Socked has been opened !");
+    connection.send("Ping..");
+    
+
+};
+
+connection.on('message', (msg: any) => {
+
+    console.log("Message received !", msg.toString());
+    
+})
