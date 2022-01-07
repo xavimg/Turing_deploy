cc4057bc21bcc7e4772e134ac63172c46e57eb84
Alex Andreba <aandrebafreelancer@gmail.com>
2022-01-07 19:01:23 +0100

    MongoDB Dockerfile + Docker image

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Turing
+
+## Dockerfile
+Build the dockerfile with\
+```docker build --build-arg TURING_DATABASE={db} --build-arg TURING_USERNAME={username} --build-arg TURING_PASSWORD={password} -t aandreba/turing . && docker run -p 27017:27017 <image_tag> ```
--- src/org/proj/db/Database.java
+++ src/org/proj/db/Database.java
@@ -6,6 +6,9 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
 import org.bson.codecs.Codec;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
@@ -13,22 +16,42 @@ import org.proj.db.codecs.PlanetarySystemCodec;
 import org.proj.db.codecs.primitive.PrimitiveProvider;
 import org.proj.game.PlanetarySystem;
 
+import java.util.Objects;
+
 public class Database {
     final private static CodecRegistry PRIMITIVES = CodecRegistries.fromProviders(PrimitiveProvider.INSTANCE);
 
     final public static MongoDatabase DB = initialize();
     final public static MongoCollection<PlanetarySystem> SYSTEMS = initializeSystems();
 
+    public static void forceInit () {
+        var a = DB;
+    }
+
     private static MongoDatabase initialize () {
-        String username = System.getenv("TURING_USERNAME");
-        String database = System.getenv("TURING_DATABASE");
-        String password = System.getenv("TURING_PASSWORD");
+        Dotenv env = Dotenv.configure().load();
+
+        String username = env.get("TURING_USERNAME");
+        String database = env.get("TURING_DATABASE");
+        String password = env.get("TURING_PASSWORD");
+
+        Objects.requireNonNull(username, "No username found");
+        Objects.requireNonNull(database, "No database found");
+        Objects.requireNonNull(password, "No password found");
 
         MongoCredential credential = MongoCredential.createCredential(username, database, password.toCharArray());
         MongoClientSettings settings = MongoClientSettings.builder().credential(credential).build();
         MongoClient client = MongoClients.create(settings);
 
-        return client.getDatabase(database);
+        MongoDatabase db = client.getDatabase(database);
+        try {
+            db.runCommand(new BsonDocument("ping", new BsonInt64(1)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return db;
     }
 
     private static CodecRegistry getRegistry (Codec codec) {
